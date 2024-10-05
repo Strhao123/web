354c2e580d33d2d44894ccbe1f15b1a27d387f38
hw_chen <905009222@qq.com>
2024-10-05 15:11:31 +0800

    初始化项目1.3

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -6,6 +6,7 @@ export const zhNavbar = navbar([
   { text: "代码笔记", icon: "code", link: "/codenotes/" },
   { text: "浮生杂记", icon: "note", link: "/floatinglife/" },
   { text: "开源项目", icon: "free", link: "/projects/" },
+
   {
     text: "资源宝库",
     icon: "advance",
@@ -23,4 +24,71 @@ export const zhNavbar = navbar([
       },
     ],
   },
+
+
+  {
+    text: "在线技术文档",
+    icon: "book",
+    children: [
+      {
+        text: "Java 8官方文档",
+        link: "https://docs.oracle.com/javase/8/docs/api/index.html",
+      },
+      {
+        text: "Spring文档中文版",
+        link: "https://www.springcloud.cc/spring-reference.html",
+      },
+      {
+        text: "Springboot官方文档",
+        link: "https://docs.spring.io/spring-boot/index.html",
+      },
+      {
+        text: "Spring Cloud官方文档",
+        link: "https://spring.io/projects/spring-cloud",
+      },
+      {
+        text: "Git中文教程",
+        link: "https://git-scm.com/book/zh/v2",
+      },
+      {
+        text: "Mybatis文档中文版",
+        link: "https://mybatis.p2hp.com/index.html",
+      },
+      {
+        text: "Mybatis-Plus文档",
+        link: "https://baomidou.com/plugins/",
+      },
+      {
+        text: "Maven文档中文版",
+        link: "https://maven.org.cn/guides/index.html",
+      },
+      {
+        text: "Tomcat官方文档",
+        link: "https://tomcat.apache.org/tomcat-10.0-doc/index.html",
+      },
+      {
+        text: "RabbitMQ官方文档",
+        link: "https://www.rabbitmq.com/docs",
+      },
+      {
+        text: "Dubbo官方文档",
+        link: "https://cn.dubbo.apache.org/zh/docs/",
+      },
+      {
+        text: "Docker官方文档",
+        link: "https://docs.docker.com/get-started/",
+      },
+      {
+        text: "Elasticsearch官方文档",
+        link: "https://www.elastic.co/guide/en/elasticsearch/reference/current/index.html",
+      },
+      {
+        text: "Netty官方文档",
+        link: "https://netty.io/wiki/index.html",
+      },
+
+    ],
+  },
+
+
 ]);
--- src/README.md
+++ src/README.md
@@ -3,8 +3,8 @@ home: true
 icon: home
 title: 个人主页
 heroImage: /head_logo.png
-heroText: 小佟の学习笔记
-tagline: ✨每星期更新一次站点✨
+heroText: hw_chenの学习笔记
+tagline: ✨不定期更新一次站点✨
 actions:
   - text: 【快速导航】
     link: /quicknav/
@@ -16,21 +16,17 @@ actions:
   - text: 【开源项目】
     link: /projects/
 features:
-  - title: 抖音
-    icon: /othericon/douyin.svg
-    details: 小佟の抖音
-    link: https://www.douyin.com/user/MS4wLjABAAAAk77SKAU4RV-_N9gURY59Bbbg82Wlbw7QQVftHhMdRPM
   - title: 码云
     icon: /othericon/gitee.svg
-    details: 小佟の码云
-    link: https://gitee.com/xt-gitee
+    details: hw_chenの码云
+    link: https://gitee.com/private_crh
   - title: B站
     icon: /othericon/bilibili.svg
-    details: 小佟のB站
-    link: https://space.bilibili.com/52459877
+    details: hw_chenのB站
+    link: https://space.bilibili.com/506956406?spm_id_from=333.1007.0.0
   - title: 力扣
     icon: /othericon/leetcode.svg
-    details: 小佟の力扣
-    link: https://leetcode.cn/u/xt-leetcode/
+    details: hw_chenの力扣
+    link: https://leetcode.cn/u/vvi2ardly-heisenberggb2/
 ---
 
